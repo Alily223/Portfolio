17232a85c599c234855dd3b96e7b71c898d53318
Austin <112780937+Alily223@users.noreply.github.com>
2022-09-28 11:16:20 -0400

    commit

--- Javascript/Audio.js
+++ Javascript/Audio.js
@@ -7,5 +7,3 @@ function Change(){
 function Change2(){
     setTimeout(() => {document.getElementById('audio').pause();}, 100)
 }
-
-https://us06st1.zoom.us/web_client/8csbj6s/html/externalLinkPage.html?ref=https://us06web.zoom.us/j/83351115352?pwd=TE9PbkdDUTNxRjMzb1YwbmVpSityZz09
