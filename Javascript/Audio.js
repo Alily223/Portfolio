eab7364f3facdc3ba8c7e42f83377e41e6011838
Austin <112780937+Alily223@users.noreply.github.com>
2022-09-27 23:37:06 -0400

    first commit

---
+++ Javascript/Audio.js
@@ -0,0 +1,11 @@
+const div = document.getElementById('Play_Button-Music');
+
+function Change(){
+ setTimeout(() => {document.getElementById('audio').play();}, 100);
+}
+
+function Change2(){
+    setTimeout(() => {document.getElementById('audio').pause();}, 100)
+}
+
+https://us06st1.zoom.us/web_client/8csbj6s/html/externalLinkPage.html?ref=https://us06web.zoom.us/j/83351115352?pwd=TE9PbkdDUTNxRjMzb1YwbmVpSityZz09
